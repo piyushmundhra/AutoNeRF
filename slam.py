2ecf7f6c59f9a7c3235dc444260b4fad27e011b8
piyushmundhra <54610591+piyushmundhra@users.noreply.github.com>
2024-07-23 14:17:51 -0700

    refactor

---
+++ slam.py
@@ -0,0 +1,373 @@
+import torch
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from glob import glob
+from depth_anything_v2 import dpt
+from SuperGlue.models.matching import Matching
+from dataclasses import dataclass
+import numpy as np
+import plotly
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+
+def get_models():
+    """
+    Initializes and returns the depth estimation model and feature matching model.
+
+    This function sets up the depth estimation model using the DepthAnythingV2 architecture
+    and loads the pre-trained weights based on the specified encoder and dataset. It also
+    configures and initializes the SuperGlue feature matching model.
+
+    Returns:
+        tuple: A tuple containing:
+            - model (DepthAnythingV2): The depth estimation model.
+            - matching (Matching): The feature matching model.
+    """
+    model_configs = {
+        'vits': {'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]},
+        'vitb': {'encoder': 'vitb', 'features': 128, 'out_channels': [96, 192, 384, 768]},
+        'vitl': {'encoder': 'vitl', 'features': 256, 'out_channels': [256, 512, 1024, 1024]}
+    }
+
+    encoder = 'vits' 
+    dataset = 'hypersim' # 'hypersim' for indoor model, 'vkitti' for outdoor model
+    max_depth = 20 # 20 for indoor model, 80 for outdoor model
+
+    model = dpt.DepthAnythingV2(**{**model_configs[encoder], 'max_depth': max_depth})
+    model.load_state_dict(torch.load(f'./Models/depth_anything_v2_metric_{dataset}_{encoder}.pth', map_location='cpu'))
+
+    config = {
+        'superpoint': {
+            'nms_radius': 4,
+            'keypoint_threshold': 0.005,
+            'max_keypoints': 1024
+        },
+        'superglue': {
+            'weights': 'indoor',
+            'sinkhorn_iterations': 20,
+            'match_threshold': 0.2,
+        }
+    }
+
+    matching = Matching(config).eval().to(torch.device('cpu'))
+
+    return model, matching
+
+@dataclass(frozen=True)
+class iPhone15ProMainCamera():
+    n: float = 4284 # px
+    center: float = 4284 / 2 # idx
+    focal_length: float = 24 # mm
+    sensor_size: float = 1/1.28 # in
+    camera_dpi: int = n / sensor_size # px / in
+    focal_length_px: float = focal_length * camera_dpi / 25.4 #px
+
+def get_rays(N=518, camera_params=iPhone15ProMainCamera()):
+    """
+    Generates a grid of rays emanating from the camera's viewpoint.
+
+    The function creates a square grid of rays based on the specified resolution N,
+    scaled according to the camera parameters. Each ray is normalized to unit length.
+
+    Parameters:
+    - N (int): The resolution of the ray grid. Determines how many rays are generated.
+    - camera_params (iPhoneCamera): An instance of the iPhoneCamera class containing
+      camera specifications.
+
+    Returns:
+    - np.ndarray: A 2D array of shape (N*N, 3), where each row represents a normalized ray
+      vector emanating from the camera's viewpoint.
+    """
+    scale = N / camera_params.n 
+    C = camera_params.center * scale
+    f = camera_params.focal_length_px * scale
+
+    rays = [np.array([(x - C), (y - C), f]) for y in range(0,N) for x in range(0,N)]
+    rays = [ray / np.linalg.norm(ray) for ray in rays]
+    return np.array(rays)
+
+def rays_to_angles(rays):
+    """
+    Converts a set of ray vectors into a 2xN^2 numpy array containing azimuth and elevation angles.
+
+    This function calculates the azimuth and elevation angles for each ray vector
+    based on their components. The azimuth angle is calculated as the arctan of the
+    x-component over the z-component. The elevation angle is calculated as the arcsin
+    of the y-component. The results are stored in a 2xN^2 numpy array where the first row
+    contains all azimuth angles and the second row contains all elevation angles.
+
+    Parameters:
+    - rays (np.ndarray): A 2D array of shape (M, 3), where M is the number of rays,
+      and each row represents a ray vector.
+
+    Returns:
+    - np.ndarray: A 2xN^2 numpy array where the first row contains azimuth angles,
+      and the second row contains elevation angles, both in degrees.
+    """
+    azimuth = np.arctan2(rays[:,0], rays[:,2]) * (180 / np.pi)
+    elevation = np.arcsin(rays[:,1]) * (180 / np.pi)
+    return np.array([azimuth, elevation])
+
+def img(path: str):
+    """
+    Reads an image from the given path, converts it to RGB, and resizes it to 518x518.
+
+    Args:
+        path (str): The path to the image file.
+
+    Returns:
+        np.ndarray: The processed image.
+    """
+    image = cv2.imread(path)  # H, W, C
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (518,518), interpolation=cv2.INTER_CUBIC)
+    return image
+
+def get_images(prefix: str):
+    """
+    Retrieves and processes all images with the given prefix from the './Images/' directory.
+
+    Args:
+        prefix (str): The prefix of the image filenames.
+
+    Returns:
+        list: A list of processed images.
+    """
+    return [img(f) for f in sorted(glob(f"./Images/{prefix}_*.jpg"))]
+
+def preprocess_image_depth(image: np.ndarray):
+    """
+    Normalizes and transposes the given image for depth processing.
+
+    Args:
+        image (np.ndarray): The input image.
+
+    Returns:
+        np.ndarray: The preprocessed image.
+    """
+    image = image / 255.0
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = (image-mean)/std
+    image = np.transpose(image, (2,0,1)) # C, H, W
+    return image
+
+def preprocess_image_matcher(image: np.ndarray) -> torch.Tensor:
+    """
+    Converts the given image to grayscale, normalizes it, and converts it to a PyTorch tensor.
+
+    Args:
+        image (np.ndarray): The input image.
+
+    Returns:
+        torch.Tensor: The preprocessed image tensor.
+    """
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    image = image / 255.0
+    image = image.astype(np.float32)
+    tensor = torch.from_numpy(image).float().to(torch.device('cpu')).unsqueeze(0).unsqueeze(0)
+    return tensor
+
+def postprocess_matches(pred: dict, num_matches: int = -1):
+    """
+    Post-processes the predicted matches to filter and sort them by confidence.
+
+    Args:
+        pred (dict): The dictionary containing prediction results.
+        num_matches (int, optional): The number of matches to return. Defaults to -1 (all matches).
+
+    Returns:
+        tuple: Filtered and sorted keypoints and matching scores.
+    """
+    pred = {k: v[0].cpu().numpy() for k, v in pred.items()}    
+    kpts0, kpts1, matches, conf = pred['keypoints0'], pred['keypoints1'], pred['matches0'], pred['matching_scores0']
+
+    valid = matches > -1
+    mkpts0 = kpts0[valid]
+    mkpts1 = kpts1[matches[valid]]
+    mconf = conf[valid]
+
+    idx = [x for x in range(len(mconf))]
+    idx = sorted(idx, key=lambda x: mconf[x])[::-1][:num_matches]
+    mkpts0 = np.array(mkpts0)[idx]
+    mkpts1 = np.array(mkpts1)[idx]
+    mconf = np.array(mconf)[idx]
+    return mkpts0, mkpts1, mconf
+
+def project_point(_x, _y, depth, N=518, camera_params=iPhone15ProMainCamera()):
+    """
+    Projects a 2D point into 3D space using depth information and camera parameters.
+
+    Args:
+        _x (int): The x-coordinate of the point.
+        _y (int): The y-coordinate of the point.
+        depth (np.ndarray): The depth map.
+        N (int, optional): The scaling factor. Defaults to 518.
+        camera_params (iPhoneCamera, optional): The camera parameters. Defaults to iPhoneCamera().
+
+    Returns:
+        tuple: The 3D coordinates (x, y, z) of the projected point.
+    """
+    scale = N / camera_params.n 
+    C = camera_params.center * scale
+    f = camera_params.focal_length_px * scale
+
+    z = depth[int(_y)][int(_x)]
+    x = -((_x - C) * z / f)
+    y = -((_y - C) * z / f)
+    return x,y,z 
+
+def get_depth(image: np.ndarray, model: torch.torch.torch.nn.Module):
+    input_image = torch.from_numpy(image).unsqueeze(0).to(torch.float32).to(torch.device("cpu"))
+    with torch.no_grad():
+        depth = model.forward(input_image).squeeze()
+        return depth
+    
+def get_matches(inputs: list[np.ndarray], model: Matching, num_matches: int = 30):
+    matches = []
+    for i in range(len(inputs) - 1):
+        with torch.no_grad():
+            pred = model({'image0': inputs[i], 'image1': inputs[i+1]})
+            matches.append(postprocess_matches(pred=pred, num_matches=num_matches))
+    return matches
+
+def project_matches_to_3d(matches, depths):
+    matches_3d = []
+    for i, (pts0, pts1, _) in enumerate(matches):
+        pts0_3d = np.array([project_point(a,b, depths[i]) for a,b in pts0])
+        pts1_3d = np.array([project_point(a,b, depths[i+1]) for a,b in pts1])
+        matches_3d.append((pts0_3d, pts1_3d))
+    return matches_3d
+
+def depth_to_pcd(depth: torch.Tensor, image: np.ndarray):
+    pcd = []
+    colors = []
+    for _y in range(0,518,2):
+        for _x in range(0,518,2):
+            pcd.append(np.array(project_point(_x,_y, depth)))
+            colors.append(image[_y][_x])
+    return np.array(pcd), np.array(colors)
+
+def get_transform(_pts0, _pts1):
+    centroid0 = np.mean(_pts0, axis=0)
+    centroid1 = np.mean(_pts1, axis=0)
+    
+    pts0 = _pts0 - centroid0
+    pts1 = _pts1 - centroid1
+    H = np.dot(pts0.T, pts1)
+
+    U, S, Vt = np.linalg.svd(H)
+
+    R = np.dot(Vt.T, U.T)
+    if np.linalg.det(R) < 0:
+        Vt[2, :] *= -1
+        R = np.dot(Vt.T, U.T)
+
+    t = np.dot(centroid0, R.T) - centroid1
+    return R, t
+
+def decompose_rotation_matrix(R):
+    sy = np.sqrt(R[0, 0] ** 2 + R[1, 0] ** 2)
+    singular = sy < 1e-6
+
+    if not singular:
+        x = np.arctan2(R[2, 1], R[2, 2])
+        y = np.arctan2(-R[2, 0], sy)
+        z = np.arctan2(R[1, 0], R[0, 0])
+    else:
+        x = np.arctan2(-R[1, 2], R[1, 1])
+        y = np.arctan2(-R[2, 0], sy)
+        z = 0
+    return np.array([
+        np.degrees(x), 
+        np.degrees(y), # elevation
+        np.degrees(z)  # azimuth
+        ])
+
+def apply_transform(points, R, t):
+    transformed_points = []
+    for point in points:
+        rotated_point = np.dot(R, np.array(point))
+        transformed_point = rotated_point + t
+        transformed_points.append(transformed_point)
+    return transformed_points
+
+def create_trace(matches, colors, opacity=0.8):
+    x = [point[0] for point in matches]
+    y = [point[1] for point in matches]
+    z = [point[2] for point in matches]
+
+    if isinstance(colors[0], (list, tuple)) and len(colors[0]) == 3:
+        marker_color = ['rgb({},{},{})'.format(*color) for color in colors]
+    else:
+        marker_color = colors
+    
+    return go.Scatter3d(
+        x=x,
+        y=y,
+        z=z,
+        mode='markers',
+        marker={
+            'size': 1,
+            'opacity': opacity,
+            'color': marker_color
+        }
+    )
+
+def create_pcd_trace(depth, image):
+    points, colors = depth_to_pcd(depth, image)
+    trace = create_trace(points, colors)  
+    return trace
+
+def create_pcd_fig(idx, depths, images):
+    fig = make_subplots(rows=1, cols=2, specs=[[{'type': 'scatter3d'}, {'type': 'scatter3d'}]])
+
+    trace1 = create_pcd_trace(depths[idx], images[idx])
+    trace2 = create_pcd_trace(depths[idx+1], images[idx+1])
+
+    fig.add_trace(trace1, row=1, col=1)
+    fig.add_trace(trace2, row=1, col=2)
+
+    fig.update_layout(height=500, width=1200)
+    return fig
+
+def plot_matches(idx, images, matches):
+    imgs = (images[idx], images[idx+1])
+    mkpts0 = matches[idx][0]
+    mkpts1 = matches[idx][1]
+    n = len(mkpts0)    
+    color = matplotlib.cm.gist_rainbow(np.linspace(start=0, stop=1.0, num=n))
+    np.random.shuffle(color)
+
+    linecolor = [(r, g, b, 0.5) for r, g, b, _ in color]
+
+    fig, ax = plt.subplots(1, 2, figsize=(8,4), dpi=100)
+    for i in range(2):
+        ax[i].imshow(imgs[i])
+        ax[i].set_aspect('equal')
+        ax[i].get_yaxis().set_ticks([])
+        ax[i].get_xaxis().set_ticks([])
+
+    plt.tight_layout()
+
+    norm = fig.transFigure.inverted()
+    fig_coords_mkpts0 = norm.transform(ax[0].transData.transform(mkpts0))
+    fig_coords_mkpts1 = norm.transform(ax[1].transData.transform(mkpts1))
+
+    fig.lines.clear()
+
+    for i in range(len(fig_coords_mkpts0)):
+        line = matplotlib.lines.Line2D(
+            xdata=[fig_coords_mkpts0[i, 0], fig_coords_mkpts1[i, 0]], 
+            ydata=[fig_coords_mkpts0[i, 1], fig_coords_mkpts1[i, 1]],  
+            transform=fig.transFigure,  
+            color=linecolor[i],  
+            linewidth=1
+        )
+        fig.lines.append(line)
+
+    ax[0].scatter(mkpts0[:, 0], mkpts0[:, 1], c=color, s=6)
+    ax[1].scatter(mkpts1[:, 0], mkpts1[:, 1], c=color, s=6)
+    return fig
